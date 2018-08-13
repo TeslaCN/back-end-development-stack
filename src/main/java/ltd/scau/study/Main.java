e145bf5bb89693866d4cde00c16efbe7b5ae5e27
TeslaCN <root496879118@me.com>
2018-08-13 23:49:40 +0800

    Initial commit

---
+++ src/main/java/ltd/scau/study/Main.java
@@ -0,0 +1,11 @@
+package ltd.scau.study;
+
+/**
+ * @author Weijie Wu
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
